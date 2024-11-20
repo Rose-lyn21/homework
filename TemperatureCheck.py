7613f021808b7de10b07b537c007cca51247cb50
Rose-lyn21 <roselynsarkodie52@gmail.com>
2024-11-20 11:41:57 +0100

    Create TemperatureCheck.py

---
+++ TemperatureCheck.py
@@ -0,0 +1,7 @@
+Temperature = int(input("What's the current Temperature"))
+if Temperature >=30 :
+    print("It's hot")
+elif 15 <= Temperature <30:
+    print("It's warm")
+else:
+    print("It's cold")
