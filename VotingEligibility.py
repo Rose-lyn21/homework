036dcde954f99f1f5c9052396c0b5dd9369a5d7e
Rose-lyn21 <roselynsarkodie52@gmail.com>
2024-11-20 14:19:44 +0100

    Create VotingEligibility.py

---
+++ VotingEligibility.py
@@ -0,0 +1,6 @@
+Country = (input("Enter the name of your country; "))
+Age = int(input("Enter your age; "))
+if Country == "Ghana" and Age >=18:
+    print("You can vote")
+else:
+    print("You cannot vote")
