f9bb924c738c89ec28b67393e4c035b5ffb40a6e
Rose-lyn21 <roselynsarkodie52@gmail.com>
2024-11-20 11:52:07 +0100

    Create GradeCalculator.py

---
+++ GradeCalculator.py
@@ -0,0 +1,11 @@
+Grade = int(input("State your score out of 100: "))
+if Grade >=90 :
+    print("A")
+elif 80 <= Grade <=89 :
+    print("B")
+elif 70 <= Grade <=79 :
+    print("C")
+elif 60 <= Grade <=69 :
+    print("D")
+else:
+    print("F")
