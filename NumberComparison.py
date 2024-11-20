c8ebdb0c0c96d8fc629bcff1255251dba41850ba
Rose-lyn21 <roselynsarkodie52@gmail.com>
2024-11-20 12:40:31 +0100

    Create NumberComparison.py

---
+++ NumberComparison.py
@@ -0,0 +1,8 @@
+Fnumber = int(input("Enter your first number; "))
+Snumber = int(input("Enter your second number; "))
+if Fnumber > Snumber:
+    print("The first number is greater")
+elif Fnumber < Snumber:
+    print("The second number is greater")
+else:
+    print("Both numbers are equal ")
